4cc4da1e76febd299f3a20cb7633b7e68d39e95b
Kirill <frolkv@gmail.com>
2022-11-22 20:03:11 +0300

    v. 0.021

--- Rates/Utilites/LocalFileManager.swift
+++ Rates/Utilites/LocalFileManager.swift
@@ -15,17 +15,41 @@ class LocalFileManager {
     
     func saveImage(image: UIImage, imageName: String, folderName: String) {
         
+        // create folder
+        createFolderIfNeeded(folderName: folderName)
+        
+        
+        // get path for imade
         guard let data = image.pngData(),
               let url = getURLForImage(imageName: imageName, folderName: folderName) else { return }
         
+        // save image to path
         do {
             try data.write(to: url)
         } catch let error {
-            print("Error saving image. \(error.localizedDescription)")
+            print("Error saving image. ImageName: \(imageName). \(error.localizedDescription)")
         }
     }
     
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        
+        guard let url = getURLForImage(imageName: imageName, folderName: folderName),
+              FileManager.default.fileExists(atPath: url.path()) else {
+        return nil
+        }
+        return UIImage(contentsOfFile: url.path())
+    }
     
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(folderName: folderName) else { return }
+        if !FileManager.default.fileExists(atPath: url.path()) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("Error creating directory. Folder name \(folderName) \(error)")
+            }
+        }
+    }
     
     private func getURLForFolder(folderName: String) -> URL? {
         guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
